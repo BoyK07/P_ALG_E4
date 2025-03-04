67d75a4f92a8d3990eaa5b7661be1d0c0dc331f4
BoyK07 <boy.krijnen@ziggo.nl>
2025-03-04 13:08:55 +0100

    Update UserSeeder to add multiple users with varied roles and credentials

--- DreamScape/database/seeders/UserSeeder.php
+++ DreamScape/database/seeders/UserSeeder.php
@@ -14,15 +14,63 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'username'          => 'BoyK07',
-            'email'             => 'admin@sadcat.space',
+            'username'          => 'ShadowSlayer',
+            'email'             => 'shadow@example.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('password'),
-            'role'              => 'admin',
-            'display_name'      => 'BoyK07',
+            'password'          => Hash::make('Test123!'),
+            'role'              => 'speler',
+            'display_name'      => 'ShadowSlayer',
+            'bio'               => null,
+            'avatar_url'        => null,
+            'remember_token'    => null,
+        ]);
+
+        User::create([
+            'username'          => 'MysticMage',
+            'email'             => 'mystic@example.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('Mage2024'),
+            'role'              => 'speler',
+            'display_name'      => 'MysticMage',
+            'bio'               => null,
+            'avatar_url'        => null,
+            'remember_token'    => null,
+        ]);
+
+        User::create([
+            'username'          => 'DragonKnight',
+            'email'             => 'dragon@example.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('Dragon!99'),
+            'role'              => 'speler',
+            'display_name'      => 'DragonKnight',
+            'bio'               => null,
+            'avatar_url'        => null,
+            'remember_token'    => null,
+        ]);
+
+        User::create([
+            'username'          => 'AdminMaster',
+            'email'             => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('Admin007'),
+            'role'              => 'beheerder',
+            'display_name'      => 'AdminMaster',
             'bio'               => 'I am the admin of this site.',
             'avatar_url'        => null,
-            'remember_token'    => null, // or you can leave this out
+            'remember_token'    => null,
+        ]);
+
+        User::create([
+            'username'          => 'ThunderRogue',
+            'email'             => 'thunder@example.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('Thund3r!!'),
+            'role'              => 'speler',
+            'display_name'      => 'ThunderRogue',
+            'bio'               => null,
+            'avatar_url'        => null,
+            'remember_token'    => null,
         ]);
     }
 }
